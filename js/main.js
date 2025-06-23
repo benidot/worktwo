ac8f152d8c069644e46d8062d0505d490c63705e
beni <193017@upf.br>
2025-06-23 16:14:20 -0300

    pre-dual

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 function toggleMode() {
   var element = document.body;
   element.classList.toggle("lightmode");
+
+  // claro que eu fui ver de complicar essa coisa
+
+  if (element.classList.contains("lightmode")) {
+    localStorage.setItem("mode", "light");
+  } else {
+    localStorage.setItem("mode", "dark");
+  }
+
+}
+
+window.onload = function() {
+  var savedMode = localStorage.getItem("mode");
+  if (savedMode === "light") {
+    document.body.classList.add("lightmode");
+  } else {
+    document.body.classList.remove("lightmode");
+  }
 }
